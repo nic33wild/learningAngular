7f8c0b239e6dd9ed65524976c02497c7433aaead
Nicola Trentini <nicola.trentini.wild@gmail.com>
2020-08-28 10:13:34 +0200

    Added router

--- firstApp/src/app/app.module.ts
+++ firstApp/src/app/app.module.ts
@@ -4,17 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { UserComponent } from './user/user.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'home', component: MainComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainComponent, UserComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
