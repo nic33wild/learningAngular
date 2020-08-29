20dbe54a50d5babffa3b090c573191286e489a70
Nicola Trentini <nicola.trentini.wild@gmail.com>
2020-08-30 01:35:46 +0200

    form example

--- firstApp/src/app/app.module.ts
+++ firstApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { UserComponent } from './user/user.component';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: 'user', component: UserComponent },
@@ -14,7 +15,12 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [AppComponent, MainComponent, UserComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- firstApp/src/app/user/user.component.ts
+++ firstApp/src/app/user/user.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
+  userForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showForm() {
+    alert(
+      'Your name is : "' +
+        this.userForm.value.name +
+        '"' +
+        ' and your email is : "' +
+        this.userForm.value.email +
+        '"'
+    );
   }
-
 }
