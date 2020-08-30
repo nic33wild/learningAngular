9b99becd60069a16ade736d99520974458662aac
Nicola Trentini <nicola.trentini.wild@gmail.com>
2020-08-30 16:07:22 +0200

    Fixed spaces in comments

--- firstApp/src/app/train/train.component.ts
+++ firstApp/src/app/train/train.component.ts
@@ -21,7 +21,7 @@ export class TrainComponent implements OnInit {
         this.trainData = value;
       });
 
-    //RxJS
+    // Example RxJS
     timer(1000, 2000)
       .pipe(map((number) => number + 1))
       .subscribe((number) => console.log(number));
@@ -29,8 +29,8 @@ export class TrainComponent implements OnInit {
 }
 
 // PATTERN OBSERVER / OBSERVABLE
-//Observable = sorgente di dati
-//Observer è una funzione che si esegue in risposta alla sorgente di dati
-//Observable.subscribers = [...] Un insieme di funzioni che aspettano di essere eseguite quando un osservable emette dei dati.
-//Observable.subscribe( observer ) Scrivendo questo si dice all'Observable di aggiungere l'observer alla sua lista di callback
+// Observable = sorgente di dati
+// Observer è una funzione che si esegue in risposta alla sorgente di dati
+// Observable.subscribers = [...] Un insieme di funzioni che aspettano di essere eseguite quando un osservable emette dei dati.
+// Observable.subscribe( observer ) Scrivendo questo si dice all'Observable di aggiungere l'observer alla sua lista di callback
 // === Observable.subscribers = [ ... , observer]
